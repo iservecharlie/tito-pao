be3d0156b5474aa317cfe924b688448b279fa3d1
iservecharlie <iservecharlie@gmail.com>
2019-07-26 19:39:49 +0000

    initializing gulp server

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const uglify = require("gulp-uglify");
+const connect = require("gulp-connect");
 const merge = require("merge-stream");
 
 // BrowserSync
@@ -108,10 +109,19 @@ function watchFiles() {
   gulp.watch("*.html", browserSyncReload);
 }
 
+function startServer() {
+    connect.server({
+        root: ["./"],
+        port: process.env.PORT || 5000, // localhost:5000
+        livereload: false
+    });
+}
+
 // Define complex tasks
 const dist = gulp.series(clean, modules);
 const build = gulp.series(dist, gulp.parallel(img, svg, css, js));
 const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
+const deploy = gulp.series(build, startServer);
 
 // Export tasks
 exports.img = img;
@@ -123,3 +133,4 @@ exports.dist = dist;
 exports.build = build;
 exports.watch = watch;
 exports.default = build;
+exports.deploy = deploy;
